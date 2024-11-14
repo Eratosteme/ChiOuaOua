4b0f3ed346498b3e5e9b28e9fab5efbb5a637c1b
Nico-Là <nicolas.marti@esme.fr>
2024-11-14 09:50:24 +0100

    Refactor Notification -> Parameters

--- app/src/main/java/com/example/chiouaoua/ui/notifications/ParametersFragment.kt
+++ app/src/main/java/com/example/chiouaoua/ui/notifications/ParametersFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.chiouaoua.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class ParametersFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val parametersViewModel =
+            ViewModelProvider(this).get(ParametersViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        parametersViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
