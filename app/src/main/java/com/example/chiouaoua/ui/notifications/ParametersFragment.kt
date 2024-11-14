9a03bf47e24c6e8c36741837394a4c80cde0cb6c
Nico-Là <nicolas.marti@esme.fr>
2024-11-14 10:39:39 +0100

    Reffactor - changement des noms

--- app/src/main/java/com/example/chiouaoua/ui/notifications/ParametersFragment.kt
+++ app/src/main/java/com/example/chiouaoua/ui/notifications/ParametersFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.chiouaoua.databinding.FragmentNotificationsBinding
+import com.example.chiouaoua.databinding.FragmentParametersBinding
 
 class ParametersFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentParametersBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,10 +25,10 @@ class ParametersFragment : Fragment() {
         val parametersViewModel =
             ViewModelProvider(this).get(ParametersViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentParametersBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
+        val textView: TextView = binding.textParameters
         parametersViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
--- app/src/main/java/com/example/chiouaoua/ui/notifications/ParametersViewModel.kt
+++ app/src/main/java/com/example/chiouaoua/ui/notifications/ParametersViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ParametersViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is Parameters Fragment"
     }
     val text: LiveData<String> = _text
 }
