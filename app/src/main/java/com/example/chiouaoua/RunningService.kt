afd73b2b349fcf8b0ffe36264a075f99ecda468a
Nico-Là <nicolas.marti@esme.fr>
2024-12-05 12:19:20 +0100

    update notifiaction

--- app/src/main/java/com/example/chiouaoua/RunningService.kt
+++ app/src/main/java/com/example/chiouaoua/RunningService.kt
@@ -4,6 +4,7 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 
 class RunningService : Service() {
 
@@ -21,8 +22,10 @@ class RunningService : Service() {
     private fun start(){
         val notification = NotificationCompat.Builder(this, "running_channel")
             .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle("Run is Active")
+            .setContentTitle("ChiOuaOua is Active")
             .setContentText("Elapsed time: 00:50")
+            .setColor(ContextCompat.getColor(this, R.color.palette1_bleu))
+            .setOngoing(true)
             .build()
         startForeground(1,notification)
     }
